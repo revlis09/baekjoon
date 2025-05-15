f32dbbd525b49a38c68a1c19f4feace32f7839cb
김가은 <revlis0909@gmail.com>
2025-05-15 17:51:05 +0900

    [Bronze V] Title: 사칙연산, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,11 +1,6 @@
 a, b=map(int, input().split())
-plus=a+b
-minus=a-b
-double=a*b
-divide=a%b
-divides=a//b
-print(plus)
-print(minus)
-print(double)
-print(divides)
-print(divide)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)

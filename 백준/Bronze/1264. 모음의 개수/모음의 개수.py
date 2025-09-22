d9091b03635e48f2896c659a6a38c524f971066b
김가은 <revlis0909@gmail.com>
2025-09-22 12:31:34 +0900

    [Bronze IV] Title: 모음의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -0,0 +1,9 @@
+while 1:
+    sum=0
+    n=input()
+    if n=="#":
+            break
+    for i in n:
+        if i == "a" or i=='e' or i=='i' or i=='o' or i =='u' or i == "A" or i=='E' or i=='I' or i=='O' or i =='U':
+            sum+=1
+    print(sum)
